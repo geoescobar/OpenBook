1808ec4d2be9bbd90355ea05b6bc8797ac9a777c
J Clancy <ukg_123@yahoo.com>
2022-07-30 12:11:05 -0500

    ROUTES AND CONTROLLER

--- server/controllers/apptController.js
+++
@@ -1,8 +0,0 @@
-const { Appt } = require('../models');
-
-const createAppt = async (data) => {
-    const apptData = await Appt.create(data)
-    return apptData;
-}
-
-module.exports = { createAppt }
--- server/models/index.js
+++
@@ -1,3 +0,0 @@
-const User = require('./User');
-const Appt = require('./Appt');
-module.exports = { User, Appt };
