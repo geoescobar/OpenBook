60e8a31f15ac41a0f5b840a06a481ad5199281c3
Paul Won <wonpaul2223@gmail.com>
2022-08-06 11:51:20 -0500

    server update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = 3001 || process.env.PORT;
-
 
 // app.use(cookieParser);
 
@@ -13,10 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-    });
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
